528892deca89cbcd6e707a4f91cfb893f4dffd73
fachiri <fachrycooles@gmail.com>
2022-09-05 18:17:46 +0800

    update responsive layout member list and about page

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,4 @@
+// navbar toggler
 $(".navToggler").click(function () {
     $(".navCollapse").toggleClass("d-block")
     $(".navCollapse").addClass("active")
@@ -5,7 +6,71 @@ $(".navToggler").click(function () {
     $(".navDismissIcon").toggleClass("d-none")
 })
 
+// news card responsiveness
 if ($(window).width() < 975) {
-    latestNewsCardHeight = $(".latestNewsCard").height() + 48 - 68
+    let latestNewsCardHeight = $(".latestNewsCard").height() + 48 - 68
     $(".rightSideNewsContainer").css("margin-top", latestNewsCardHeight+"px")
 }
+
+// scrollable with button for memberLists
+let scrollDuration = 300;
+let leftPaddle = $(".left-paddle")
+let rightPaddle = $(".right-paddle")
+let itemsLength = $('.memberItem').length;
+let itemSize = $('.memberItem').outerWidth(true);
+let paddleMargin = 20;
+
+let getMenuWrapperSize = function() {
+	return $('.memberWrapper').outerWidth();
+}
+
+let menuWrapperSize = getMenuWrapperSize();
+
+$(window).on('resize', function() {
+	menuWrapperSize = getMenuWrapperSize();
+});
+
+let menuVisibleSize = menuWrapperSize;
+let itemVisible = Math.floor(menuVisibleSize / itemSize)
+
+console.log("menuVisibleSize : "+ (menuVisibleSize - 96));
+console.log("itemSize : "+itemSize);
+console.log("itemVisible : "+itemVisible);
+
+// some responsive behav for memberItem
+itemSize = (( menuVisibleSize - 96 ) / itemVisible)
+newItemSize = itemSize - 20
+$('.memberItem img').css("width", newItemSize+"px")
+
+let getMenuSize = function() {
+	return itemsLength * itemSize;
+};
+
+let menuSize = getMenuSize();
+let menuInvisibleSize = menuSize - menuWrapperSize;
+let getMenuPosition = function() {
+	return $('.memberLists').scrollLeft();
+};
+    $('.memberLists').on('scroll', function() {
+        menuInvisibleSize = menuSize - menuWrapperSize;
+        let menuPosition = getMenuPosition();
+        let menuEndOffset = menuInvisibleSize - paddleMargin;
+        if (menuPosition <= paddleMargin) {
+            $(leftPaddle).addClass('d-none');
+            $(rightPaddle).removeClass('d-none');
+        } else if (menuPosition < menuEndOffset) {
+            $(leftPaddle).removeClass('d-none');
+            $(rightPaddle).removeClass('d-none');
+        } else if (menuPosition >= menuEndOffset) {
+            $(leftPaddle).removeClass('d-none');
+            $(rightPaddle).addClass('d-none');
+    }
+});
+
+$(rightPaddle).on('click', function() {
+	$('.memberLists').animate( { scrollLeft: getMenuPosition() + itemSize}, scrollDuration);
+});
+
+$(leftPaddle).on('click', function() {
+	$('.memberLists').animate( { scrollLeft: getMenuPosition() - itemSize }, scrollDuration);
+});
